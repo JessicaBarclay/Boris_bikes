ca8cac9d4d6edce6f2f7be971bfc83aec2a4caf4
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-04-11 17:01:08 +0100

    Added full? private method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,18 +4,23 @@ class DockingStation
     attr_reader :bike
 
     def initialize()
-    @bikes = []
-
+      @bikes = []
     end
 
     def release_bike
-    fail "No bikes available" if  @bikes.empty?
+    fail "No bikes available" if @bikes.empty?
       @bikes.pop
     end
 
     def dock(bike)
       fail "Maximum number of bikes" if @bikes.count >= 20
       @bikes << bike
+    end
+
+    private
+
+    def full?
+      !dock(bike).empty? ? true : false
     end
 
 end
